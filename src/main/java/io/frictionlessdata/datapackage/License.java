40ce3c40967864b5bf1f4f4de87709df14b9b39b
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2025-06-16 09:07:33 +0200

    Add additionalProperties fields to License and Source

--- src/main/java/io/frictionlessdata/datapackage/License.java
+++ src/main/java/io/frictionlessdata/datapackage/License.java
@@ -1,11 +1,21 @@
 package io.frictionlessdata.datapackage;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class License {
     private String name;
 
     private String path;
 
     private String title;
+
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<>();
 
     public String getName() {
         return name;
@@ -29,5 +39,15 @@ public class License {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
     }
 }
--- src/main/java/io/frictionlessdata/datapackage/Source.java
+++ src/main/java/io/frictionlessdata/datapackage/Source.java
@@ -1,10 +1,20 @@
 package io.frictionlessdata.datapackage;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Source {
 
     private String path;
 
     private String title;
+
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<>();
 
     public String getPath() {
         return path;
@@ -20,5 +30,15 @@ public class Source {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
     }
 }
